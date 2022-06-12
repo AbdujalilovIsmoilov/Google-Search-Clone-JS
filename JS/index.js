b05dc2b922766be1946b7315e04176b41d7a7477
= <ismoilbek20050421@gmail.com>
2022-06-12 15:22:09 +0500

    Google Search Clone JS

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+// /========================================== Google JavaScript to the codes Start ============================== \
+let search = document.getElementById("search");
+let btn = document.getElementById("btn");
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let url = `https://www.google.com/search?q=${search.value}`;
+  window.open(url);
+  search.value = "";
+});
+// /========================================== Google JavaScript to the codes End ============================== \
